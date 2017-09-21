08fd28b2c15c54bf0b24c72a77d722665709813d
folknor <folk@folk.wtf>
2017-09-21 22:07:43 +0200

    Initial upload for Stellaris 1.6.x or earlier.

---
+++ modinfo.lua
@@ -0,0 +1,9 @@
+-- modinfo read by stlrel from http://github.com/stellaris-mods/scripts
+return {
+	path = "folk_stationyearjeez",
+	name = "Yet Another Station Buff, Jeez",
+	tags = { "Military" },
+	picture = "thumb.png",
+	supported_version = "1.6.*",
+	remote_file_id = 839507693,
+}
